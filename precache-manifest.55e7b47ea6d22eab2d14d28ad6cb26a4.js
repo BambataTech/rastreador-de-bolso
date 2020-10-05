b1f05285538e2af410883898abb9d1307ec0dfba
Vitor Duarte <vitor.alves.duarte@gmail.com>
2020-10-04 23:37:31 -0300

    Updates

---
+++ precache-manifest.55e7b47ea6d22eab2d14d28ad6cb26a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b26fb4308fa7d2be1c181583fb4ecfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b34c8472cada01efd09",
+    "url": "/static/css/main.f5d232e8.chunk.css"
+  },
+  {
+    "revision": "bb0924e5948ad8f1df74",
+    "url": "/static/js/2.0b1865cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b1865cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b34c8472cada01efd09",
+    "url": "/static/js/main.a36a2506.chunk.js"
+  },
+  {
+    "revision": "26bd18d7143fca5bff13",
+    "url": "/static/js/runtime-main.7a02ad32.js"
+  }
+]);
